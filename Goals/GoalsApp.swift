f8afcd2645d3a05ae047986800433110e4d30047
“jeremyfd” <jdaines99@gmail.com>
2023-12-24 08:12:01 +0000

    Initial Commit

---
+++ Goals/GoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsApp.swift
+//  Goals
+//
+//  Created by Work on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
