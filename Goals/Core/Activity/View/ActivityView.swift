d25e0e5d06c8dc4b1e5075248f8d19d3076fefc7
Jeremy Daines <jdaines99@gmail.com>
2024-02-22 11:12:25 +0000

    Fixed navigation stack issues for ActivtyView

--- Goals/Core/Activity/View/ActivityView.swift
+++ Goals/Core/Activity/View/ActivityView.swift
@@ -16,81 +16,79 @@ struct ActivityView: View {
     @StateObject var viewModel = ActivityViewModel()
     @State private var selectedNavigation: ActivityNavigationConfig?
     @State private var isNavigationTriggered: Bool = false
-
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    ActivityFilterView(selectedFilter: $viewModel.selectedFilter)
-                        .padding(.vertical)
-
-                    LazyVStack(spacing: 16) {
-                        ForEach(viewModel.notifications) { activityModel in
-                            if activityModel.type != .friend {
-                                Button(action: {
-                                    self.selectedNavigation = .activityDetail(activityModel)
-                                    self.isNavigationTriggered = true
-                                }) {
-                                    ActivityRowView(model: activityModel)
-                                }
-                            } else if let user = activityModel.user {
-                                Button(action: {
-                                    self.selectedNavigation = .userProfile(user)
-                                    self.isNavigationTriggered = true
-                                }) {
-                                    ActivityRowView(model: activityModel)
-                                }
+        VStack {
+            ScrollView {
+                ActivityFilterView(selectedFilter: $viewModel.selectedFilter)
+                    .padding(.vertical)
+                
+                LazyVStack(spacing: 16) {
+                    ForEach(viewModel.notifications) { activityModel in
+                        if activityModel.type != .friend {
+                            Button(action: {
+                                self.selectedNavigation = .activityDetail(activityModel)
+                                self.isNavigationTriggered = true
+                            }) {
+                                ActivityRowView(model: activityModel)
+                            }
+                        } else if let user = activityModel.user {
+                            Button(action: {
+                                self.selectedNavigation = .userProfile(user)
+                                self.isNavigationTriggered = true
+                            }) {
+                                ActivityRowView(model: activityModel)
                             }
                         }
                     }
                 }
-                .overlay {
-                    if viewModel.isLoading {
-                        ProgressView()
-                    }
-                }
-                .navigationTitle("Activity")
             }
-            // Conditional NavigationLinks for each case
-            .background(
-                EmptyView()
-                    .background(
-                        NavigationLink(
-                            destination: destinationView(),
-                            isActive: $isNavigationTriggered
-                        ) { EmptyView() }
-                    )
-            )
+            .overlay {
+                if viewModel.isLoading {
+                    ProgressView()
+                }
+            }
+            .navigationTitle("Activity")
+            .navigationBarTitleDisplayMode(.inline)
         }
+        // Conditional NavigationLinks for each case
+        .background(
+            EmptyView()
+                .background(
+                    NavigationLink(
+                        destination: destinationView(),
+                        isActive: $isNavigationTriggered
+                    ) { EmptyView() }
+                )
+        )
     }
-
-        @ViewBuilder
-        private func destinationView() -> some View {
-            if let navigation = selectedNavigation {
-                switch navigation {
-                case .activityDetail(let activity):
-                    if let goal = activity.goal {
-                        ExpandedGoalView(goal: goal)
-                            .onAppear {
-                                self.isNavigationTriggered = false
-                            }
-                    } else {
-                        Text("Goal details are not available.")
-                            .onAppear {
-                                self.isNavigationTriggered = false
-                            }
-                    }
-                case .userProfile(let user):
-                    UserProfileView(user: user)
+    
+    @ViewBuilder
+    private func destinationView() -> some View {
+        if let navigation = selectedNavigation {
+            switch navigation {
+            case .activityDetail(let activity):
+                if let goal = activity.goal {
+                    ExpandedGoalView(goal: goal)
+                        .onAppear {
+                            self.isNavigationTriggered = false
+                        }
+                } else {
+                    Text("Goal details are not available.")
                         .onAppear {
                             self.isNavigationTriggered = false
                         }
                 }
-            } else {
-                EmptyView()
+            case .userProfile(let user):
+                UserProfileView(user: user)
+                    .onAppear {
+                        self.isNavigationTriggered = false
+                    }
             }
+        } else {
+            EmptyView()
         }
-
+    }
 }
 
 
