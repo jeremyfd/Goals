5d7cf907658929e31d5c89a6d6bf1313e0d20599
Jeremy Daines <jdaines99@gmail.com>
2024-03-05 16:08:45 +0000

    New icon

--- Goals/Core/Public Challenges/View/PublicChallengesView.swift
+++ Goals/Core/Public Challenges/View/PublicChallengesView.swift
@@ -9,19 +9,43 @@ import SwiftUI
 
 struct PublicChallengesView: View {
     var body: some View {
+//        NavigationStack{
+//            
+//            LinearGradientView()
+//                .ignoresSafeArea()
+//                .overlay(
+//                    VStack {
+//                        Text("")
+//                            .padding()
+//                        
+//                        Text("")
+//                            .padding()
+//                        
+//                        Text("Phylax")
+//                            .font(.system(size: 100))
+//                            .fontWeight(.bold)
+//                            .padding(.vertical)
+//                        
+//                        Spacer()
+//                    }
+//                )
+//            
+//        }
         
-        VStack{
-            Text("Public Challenges")
-                .font(.title)
-                .fontWeight(.bold)
-            Text("Coming soon!")
-                .padding(.bottom)
-            
-            Text("Go to the Gym")
-            Text("Wake up at 6am")
-            Text("David Goggins Challenge")
-            Text("SimplyPiano Challenge")
-        }
+                VStack{
+                    Text("Public Challenges")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("Coming soon!")
+                        .padding(.bottom)
+        
+                    Text("Go to the Gym")
+                    Text("Wake up at 6am")
+                    Text("David Goggins Challenge")
+                    Text("SimplyPiano Challenge")
+                }
+        
+        
     }
 }
 
