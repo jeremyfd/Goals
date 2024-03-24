2c25c562fcb83f35f867d250f8eed2bafc6a5635
Jeremy Daines <jdaines99@gmail.com>
2024-03-24 12:56:46 +0000

    Added notifcations 15h before deadline for goal owner

--- functions/index.js
+++ functions/index.js
@@ -110,3 +110,46 @@ exports.sendUserNotification = functions.firestore
         throw new functions.https.HttpsError('unknown', `Failed to send notification: ${error.message}`);
     }
 });
+
+// eslint-disable-next-line no-unused-vars
+exports.checkStepDeadlines = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
+    // Current date and time
+    const now = new Date();
+    // Date and time for 15 hours from now
+    const fifteenHoursLater = new Date(now.getTime() + (15 * 60 * 60 * 1000));
+
+    // Reference to the steps collection
+    const stepsRef = admin.firestore().collection('steps');
+
+    // Fetch steps whose deadline is within the next 15 hours
+    const stepsSnapshot = await stepsRef.get();
+
+    stepsSnapshot.forEach(async (doc) => {
+        const step = doc.data();
+        const stepDeadline = step.deadline.toDate(); // Convert Firestore Timestamp to JavaScript Date
+
+        if (stepDeadline <= fifteenHoursLater && stepDeadline > now) {
+            // Lookup the user associated with the step
+            const userRef = admin.firestore().collection('users').doc(step.ownerUid);
+            const userSnapshot = await userRef.get();
+            const user = userSnapshot.data();
+
+            // If the user has an FCM token, send them a notification
+            if (user && user.fcmToken) {
+                const message = {
+                    notification: {
+                        title: 'Step Deadline Approaching',
+                        body: `Your step with deadline ${stepDeadline} is almost due.`,
+                    },
+                    token: user.fcmToken,
+                };
+
+                admin.messaging().send(message).then((response) => {
+                    console.log('Successfully sent message:', response);
+                }).catch((error) => {
+                    console.error('Error sending message:', error);
+                });
+            }
+        }
+    });
+});
